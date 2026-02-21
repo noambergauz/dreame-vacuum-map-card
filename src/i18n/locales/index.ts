837e70ab3532d85bc37c42f51c2e815a3bd512ab
nekoprpr <48119840+nekoprpr@users.noreply.github.com>
2026-02-22 04:28:31 +0800

    Add Chinese localization support

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,11 +1,13 @@
 import { en } from './en';
 import { de } from './de';
 import { ru } from './ru';
+import { zh } from './zh';
 
 export const locales = {
   en,
   de,
   ru,
+  zh,
 };
 
 export type SupportedLanguage = keyof typeof locales;
